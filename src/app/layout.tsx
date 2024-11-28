aea528b0438d7d09d333ce2fb350352873b210eb
senul ranasinghe <hardlove.mrplus@gmail.com>
2024-11-28 21:29:51 +0530

    feat(layout): added main frame to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Image from 'next/image'
 import "./globals.css";
+import Footer from "@/components/footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +30,20 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+    <div className="min-h-screen bg-white py-8 px-4 mb-12">
+      <div className="max-w-7xl mx-auto">
+        <div className="flex flex-col items-center justify-center">
+          <Image
+            src="/logo.png"
+            alt="Matrix Institute Logo"
+            width={150}
+            height={75}
+          />
+        </div>
         {children}
+        </div>
+        </div>
+        <Footer/>
       </body>
     </html>
   );
